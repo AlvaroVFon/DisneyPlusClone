4555d91f04ac976ab908cecfb92653909d36b2f0
Álvaro Villamarín <alvaro_90_xti@hotmail.com>
2024-01-17 23:40:09 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <Header /> {/*Nav inside*/}
+      <main>
+        <section></section>
+        <section></section>
+      </main>
+    </>
+  )
+}
+
+export default App
