27f414803b8229057bd26a9ffebe91241309c654
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-01-18 21:48:31 +0100

    arreglado margin Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,7 +3,7 @@
 export const Button = ({ label = 'Iniciar Sesión', onClick }) => {
 
   return (
-    <button className=" relative p-3 font-normal text-xl
+    <button className=" relative px-4 py-2 font-normal text-xl mr-10 mt-2
      text-white uppercase border border-white rounded-md bg-black/70 
      tracking-wide before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-black/5
      hover:text-black hover:bg-white" 
