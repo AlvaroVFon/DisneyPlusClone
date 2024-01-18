05fc3082f97f2d7184432046d546f653059f0bfc
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-01-18 21:35:56 +0100

    teminado header, comenzada section primera

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ function App() {
 
   return (
     <>
+      <main className='bg-[url("/bgImage.jpg")] h-screen' >
       <Header />
-      <main className='bg-[url("/bgImage.jpg")] h-screen -mt-16' >
-        <section></section>
+        <section className='w-full flex flex-col items-center justify-center min-h-[70vh] text-white space-y-3'>
+          <p className=' text-[1.70rem] w-[500px] text-center font-medium'>
+            Contenidos Originales exclusivos, películas de éxito y series para maratonear
+          </p>
+          <p className='text-md'>Cancela cuando quieras.*</p>
+          <p className='text-[#B3B4B5]'>Escribe tu dirección de correo electrónico para crear o reactivar tu suscripción.</p>
+        </section>
         <section></section>
       </main >
     </>
@@ -13,3 +19,4 @@ function App() {
 }
 
 export default App
+ 
