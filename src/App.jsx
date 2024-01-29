1bf5d43011058a29869a49e26c008d8271b02049
Álvaro Villamarín <alvaro_90_xti@hotmail.com>
2024-01-29 21:53:47 +0100

    arreglado after:bg App.jsx, terminado sección FAQs

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
 
   return (
     <>
-      <main className='bg-[url("/bgImage.jpg")] bg-cover min-h-screen after:content-[""] after:absolute after:left-0 after:bottom-0 after:h-full after:w-full after:bg-gradient-to-t after:from-[#050614] after:via-40% after:via-[#040714]/90 after:to-transparent after:z-10' >
+      <main className='bg-[url("/bgImage.jpg")] bg-cover min-h-screen after:content-[""] after:absolute after:left-0 after:bottom-0 after:h-full after:w-full after:bg-gradient-to-t after:from-[#050614] after:via-40% after:via-[#040714]/90 after:to-transparent after:z-10 md:after:hidden' >
         <Header />
         <Landing id="PrimeSection" />
       </main >
       <PrimeSection />
       <PaymentSection />
-      <Enjoy/>
-      <FAQs/>
+      <Enjoy />
+      <FAQs />
     </>
   )
 }
